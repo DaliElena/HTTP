219bb4ad800404cb094bc0c0adadc13422a37dd3
Lena <lena.nigoritsa@mail.ru>
2021-10-17 14:35:26 +0300

    Python queue

--- Python/server.py
+++ Python/server.py
@@ -1,39 +1,18 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from io import BytesIO
-
-class Queue: 
- 
-  def __init__(self): 
-      self.queue = list() 
- 
-  def add_element(self,val): 
-      if val not in self.queue: 
-          self.queue.insert(0,val) 
-          return True 
-      return False 
- 
-  def remove_element(self): 
-     if len(self.queue)<1: 
-         return None
-     return self.queue.pop(0)
-
-  def size(self): 
-      return len(self.queue)
-  def display(self):
-      next_item = ''
-      while len(self.queue)>0:
-          next_item += str(self.queue.pop()) + '\n'
-      return next_item
-      
-
+import queue
 
 global que
-que = Queue()
-que.add_element("January") 
-que.add_element("February") 
-que.add_element("March")
-print(que.display())
+que = queue.Queue(5)
+que.put('A') 
+que.put('B') 
+que.put('C')
 
+def display(self):
+      next_item = ''
+      for i in self.queue:
+          next_item += str(i) + '\n'
+      return next_item
 
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
@@ -43,13 +22,21 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
-        self.wfile.write(que.display())
+        out_data = bytes(display(que), encoding = 'utf-8')
+        self.wfile.write(out_data)
         
 
     # определяем метод `do_POST` 
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
         body = self.rfile.read(content_length)
+        data=body.decode('UTF-8')
+        if data!='':
+            if que.full():
+                que.get()
+                que.put(data)        
+            else:
+                que.put(data)
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
